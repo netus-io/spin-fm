c291bf3161fbb8bd1d78f8196a29d22359dc3279
julianapeace <chancecordelia@gmail.com>
2019-02-16 01:33:19 -0600

    removed index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Route, Switch, Redirect, Link } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import './index.css';
 
 
 import store from './reducers'
